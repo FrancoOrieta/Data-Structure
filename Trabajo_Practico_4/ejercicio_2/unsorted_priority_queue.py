ce0e100741331ddfcd77652c56aa4e04a810e12e
Franco Orieta <orieta.francojavier@gmail.com>
2022-10-27 13:07:07 -0300

    added methods add,len,str and init

--- Trabajo_Practico_4/ejercicio_2/unsorted_priority_queue.py
+++ Trabajo_Practico_4/ejercicio_2/unsorted_priority_queue.py
@@ -1,4 +1,32 @@
 from unsorted_priority_queue_abstract import UnsortedPriorityQueueAbstract
+from typing import Any
 
 class UnsortedPriorityQueue(UnsortedPriorityQueueAbstract):
     
+    def __init__(self):
+        self._element = []
+        self._size = 0
+    
+    def add(self, k: Any, v: Any) -> None:
+        elemento = (k,v)
+        self._element.append(elemento)
+        self._size += 1
+
+    def is_empty(self) -> bool:
+        return self._size == 0
+
+    def __str__(self) -> str:
+
+        if self.is_empty():
+            return "UnsortedPriorityQueue()"
+
+        res = ""
+        for i in range(0,self._element.__len__()):
+            res += str(self._element[i]) + ","
+
+        res = res[:-1]
+
+        return f"UnsortedPriorityQueue({res})"
+    
+    def __len__(self) -> int:
+        return self._size
---
+++ Trabajo_Practico_4/ejercicio_2/unsorted_priority_queue_client.py
@@ -0,0 +1,10 @@
+from unsorted_priority_queue import UnsortedPriorityQueue
+
+cola_prioridad = UnsortedPriorityQueue()
+
+cola_prioridad.add("Messi",10)
+cola_prioridad.add("CR7",7)
+cola_prioridad.add("Chocolate","Blanco")
+
+print(cola_prioridad)
+print(cola_prioridad.__len__())
