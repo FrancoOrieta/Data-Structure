0cd2413548eae59d849dc2b5d5ddc07c825814a1
Franco Orieta <orieta.francojavier@gmail.com>
2022-10-28 10:31:37 -0300

    leaving the client pretty

--- Trabajo_Practico_4/ejercicio_3/priority_queue_stack_client.py
+++ Trabajo_Practico_4/ejercicio_3/priority_queue_stack_client.py
@@ -10,8 +10,12 @@ print("Llenando la pila".center(36, "*"))
 print("Cargando...\n")
 llenado(stack)
 
+print("Pila vacia?".center(26, "*"))
+print(stack.is_empty(),"\n")
+
 print("Longitud".center(26, "*"))
 print(len(stack),"\n")
+
 
 print("Pila".center(20, "*"))
 print(stack,"\n")
