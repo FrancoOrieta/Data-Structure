89fcd6c2ea6ba187df1ee433502b4505dc160e8c
Franco Orieta <orieta.francojavier@gmail.com>
2022-10-28 12:24:35 -0300

    added heap queue

--- Trabajo_Practico_4/ejercicio_4/heap_queue.py
+++ Trabajo_Practico_4/ejercicio_4/heap_queue.py
@@ -1,4 +1,26 @@
+from typing import Any
 from python_ed_fcad_uner.data_structures import ArrayHeap
 
 class HeapQueue(ArrayHeap):
-    pass
+
+    KEY = 1
+
+    @classmethod
+    def increment_key(cls):
+        cls.KEY += 1
+    
+    @classmethod
+    def decrecrement_key(cls):
+        cls.KEY -= 1
+
+    def enqueue(self, elem: Any):
+        self.add(self.KEY, elem)
+        self.increment_key()
+
+    def dequeue(self):
+        min = self.remove_min()[1]
+        self.decrecrement_key()
+        return min
+
+    def first(self):
+        return self.min()
