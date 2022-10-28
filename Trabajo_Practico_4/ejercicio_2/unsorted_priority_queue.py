3595771df4da6a416c2ed0d15388303421921a05
Franco Orieta <orieta.francojavier@gmail.com>
2022-10-28 09:35:29 -0300

    commenting the code

--- Trabajo_Practico_4/ejercicio_2/unsorted_priority_queue.py
+++ Trabajo_Practico_4/ejercicio_2/unsorted_priority_queue.py
@@ -4,10 +4,12 @@ from typing import Any, Tuple
 class UnsortedPriorityQueue(UnsortedPriorityQueueAbstract):
     
     def __init__(self):
+        #Los elementos de la cola están implementados  con una lista
         self._element = []
         self._size = 0
     
     def add(self, k: Any, v: Any) -> None:
+        #Cada elemento es una tupla, con una clave k y un valor v
         elemento = (k,v)
         self._element.append(elemento)
         self._size += 1
@@ -35,29 +37,34 @@ class UnsortedPriorityQueue(UnsortedPriorityQueueAbstract):
 
         if self.is_empty():
             raise Exception("Operación no soportada, la estructura está vacía.")
-
+        
+        #min_elemento: la usamos como variable de busqueda
+        #Toma el valor de la primer tupla de la lista
         min_elemento = self._element[0]
 
+        #Recorremos toda la lista
         for i in range(0,len(self._element)):
 
+            #min_elemento[0]: Hacemos referencia a la k en la tupla
+            #self_element[i][0]: Con [i] referenciamos una tupla de la lista y con [0] referenciamos la k de la tupla
+            #True: significa que aun no hayamos el min_valor, cambiamos min_elemento por la tupla [i] de la lista
             if min_elemento[0] > self._element[i][0]:
                 min_elemento = self._element[i]
         
-        return min_elemento
+        return min_elemento #retornamos la tupla con menor clave
     
     def remove_min(self) -> Tuple[Any]:
+
         if self.is_empty():
             raise Exception("Operación no soportada, la estructura está vacía.")
+        
+        #llamamos a la funcion min para obtener el minimo elemento
+        min_elemento = self.min()
 
-        min_elemento = self._element[0]
-
-        for i in range(0,len(self._element)):
-
-            if min_elemento[0] > self._element[i][0]:
-                min_elemento = self._element[i]
-
+        #la funcion de lista (lista.index(A)): retorna el indice del elemento A en la lista
+        #guardamos el indice del elemento con menor clave
         indice = self._element.index(min_elemento)
         
-        min_remove = self._element.pop(indice)
-        self._size -= 1
+        min_remove = self._element.pop(indice) #Eliminamos el elemento minimo y lo retornamos
+        self._size -= 1     #Decrece el tamaño de la cola
         return min_remove
