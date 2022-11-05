9de191f62b6229804e4bf7eef22de40b3d2fa6a0
Franco Orieta <orieta.francojavier@gmail.com>
2022-11-05 15:27:05 -0300

    removing the comments

--- Trabajo_Practico_4/ejercicio_2/unsorted_priority_queue.py
+++ Trabajo_Practico_4/ejercicio_2/unsorted_priority_queue.py
@@ -41,33 +41,24 @@ class UnsortedPriorityQueue(UnsortedPriorityQueueAbstract):
         if self.is_empty():
             raise Exception("Operación no soportada, la estructura está vacía.")
         
-        #min_elemento: la usamos como variable de busqueda
-        #Toma el valor de la primer tupla de la lista
         min_elemento = self._element[0]
 
-        #Recorremos toda la lista
         for i in range(0,len(self._element)):
 
-            #min_elemento[0]: Hacemos referencia a la k en la tupla
-            #self_element[i][0]: Con [i] referenciamos una tupla de la lista y con [0] referenciamos la k de la tupla
-            #True: significa que aun no hayamos el min_valor, cambiamos min_elemento por la tupla [i] de la lista
             if min_elemento[0] > self._element[i][0]:
                 min_elemento = self._element[i]
         
-        return min_elemento #retornamos la tupla con menor clave
+        return min_elemento
     
     def remove_min(self) -> Tuple[Any]:
 
         if self.is_empty():
             raise Exception("Operación no soportada, la estructura está vacía.")
-        
-        #llamamos a la funcion min para obtener el minimo elemento
+
         min_elemento = self.min()
 
-        #la funcion de lista (lista.index(A)): retorna el indice del elemento A en la lista
-        #guardamos el indice del elemento con menor clave
         indice = self._element.index(min_elemento)
         
-        min_remove = self._element.pop(indice) #Eliminamos el elemento minimo y lo retornamos
-        self._size -= 1     #Decrece el tamaño de la cola
+        min_remove = self._element.pop(indice)
+        self._size -= 1 
         return min_remove
